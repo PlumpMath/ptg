d08eca4e98d1dc3b723c82d13e0cd89f984ddfd9
John Jacobsen <john@mail.npxdesigns.com>
2015-02-07 22:15:00 -0600

    Starting to actually work.

--- src/ptg/core.clj
+++ src/ptg/core.clj
@@ -8,32 +8,34 @@
       img (img/load-image (clojure.java.io/resource filename))
       image-width (.getWidth img)
       image-height (.getHeight img)
-      pixels-between-lines 80
-      lines-across (int (/ image-width pixels-between-lines))
-      lines-down (int (/ image-height pixels-between-lines))
+      support-width 11
+      support-height 8.5
+      support-grid-spacing 0.5
+      lines-across (int (/ support-width support-grid-spacing))
+      lines-down (int (/ support-height support-grid-spacing))
+      pixels-between-lines (int (/ image-width lines-across))
       line-x (fn [y x0 x1]
                (doseq [x (range x0 x1)]
                  (.setRGB img x y 0)))
       line-y (fn [x y0 y1]
                (doseq [y (range y0 y1)]
-                 (.setRGB img x y 0)))]
+                 (.setRGB img x y 0)))
+      img-scroll (see/scrollable (see/label :icon img))
+      input-form (see/horizontal-panel
+                  :items [(see/label :text " Support size: ")
+                          (see/text :text (str support-width))
+                          (see/label :text " inches wide by ")
+                          (see/text (str support-height))
+                          (see/label :text " inches high. Square size: ")
+                          (see/text (str support-grid-spacing))
+                          (see/label :text " inch ")])
+      contents (see/vertical-panel :items [input-form
+                                           img-scroll])
+      f (see/frame :content contents
+                   :title filename)]
   (doseq [x (map (partial * pixels-between-lines) (range lines-across))]
     (line-y x 0 image-height))
   (doseq [y (map (partial * pixels-between-lines) (range lines-down))]
     (line-x y 0 image-width))
-  (let [img-scroll (see/scrollable (see/label :icon img))
-        input-form (see/horizontal-panel
-                    :items [(see/label :text "Support size: ")
-                            (see/text :columns 1 :text "11")
-                            (see/label :text " inches wide by ")
-                            (see/text "8.5")
-                            (see/label :text " inches high. Square size: ")
-                            (see/text "1")
-                            (see/label :text " inch.")])
-        contents (see/vertical-panel :items [input-form
-                                             img-scroll])
-        f (see/frame :content contents
-                     :minimum-size [640 :by 480]
-                     :title filename)]
-    (see/pack! f)
-    (see/show! f)))
+  (see/pack! f)
+  (see/show! f))
