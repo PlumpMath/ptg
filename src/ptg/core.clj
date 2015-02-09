85e694b0c5d80b83868f3e7a5111f8504f0605c5
John Jacobsen <john@mail.npxdesigns.com>
2015-02-08 19:57:44 -0600

    First version that correctly grids!

--- src/ptg/core.clj
+++ src/ptg/core.clj
@@ -19,34 +19,50 @@
         image-width (.getWidth img)
         image-height (.getHeight img)
         support-width 11
-        support-height 8.5
-        support-grid-spacing 0.5
-        lines-across (int (/ support-width support-grid-spacing))
-        lines-down (int (/ support-height support-grid-spacing))
-        pixels-between-lines (int (/ image-width lines-across))
+        support-height 5
+        support-grid-spacing 1
+
+        ppi (min (/ image-width support-width)
+                 (/ image-height support-height))
+
+        pixels-per-square (* ppi support-grid-spacing)
+
+        lines-across (/ (* support-width ppi) pixels-per-square)
+        lines-down (/ (* support-height ppi) pixels-per-square)
+
+        ;; Only generate grid covering canvas/support area...
+        pixels-horiz-on-support (* ppi support-width)
+        pixels-vert-on-support (* ppi support-height)
+        margin-side (/ (- image-width pixels-horiz-on-support) 2)
+        margin-top (/ (- image-height pixels-vert-on-support) 2)
+
         img-scroll (see/scrollable (see/label :icon img))
         input-form (see/horizontal-panel
                     :items [(see/label :text " Support size: ")
                             (see/text :text (str support-width))
-                            (see/label :text " inches wide by ")
+                            (see/label :text " inches/cm wide by ")
                             (see/text (str support-height))
-                            (see/label :text " inches high. Square size: ")
+                            (see/label :text " inches/cm high. Square size: ")
                             (see/text (str support-grid-spacing))
-                            (see/label :text " inch ")])
+                            (see/label :text " inch/cm. ")])
         contents (see/vertical-panel :items [input-form
                                              img-scroll])
         f (see/frame :content contents
                      :title (.getFile file-ob))]
-    (doseq [x (map (partial * pixels-between-lines) (range lines-across))]
-      (line-y img x 0 image-height))
-    (doseq [y (map (partial * pixels-between-lines) (range lines-down))]
-      (line-x img y 0 image-width))
+    (doseq [x (map #(+ margin-side (* pixels-per-square %))
+                   (range lines-across))]
+      (line-y img x margin-top (+ margin-top pixels-vert-on-support)))
+    (line-y img
+            (dec (+ margin-side pixels-horiz-on-support))
+            margin-top (+ margin-top pixels-vert-on-support))
+    (doseq [y (map #(+ margin-top (* pixels-per-square %))
+                   (range lines-down))]
+      (line-x img y margin-side (+ margin-side pixels-horiz-on-support)))
+    (line-x img
+            (dec (+ margin-top pixels-vert-on-support))
+            margin-side (+ margin-side pixels-horiz-on-support))
     (see/pack! f)
     (see/show! f)))
 
 
-(comment
-
-  (dogrid (clojure.java.io/resource "img.jpg"))
-
-)
+(dogrid (clojure.java.io/resource "img.jpg"))
