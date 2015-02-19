e7b3ad950b00b7af0e543087bd0b17cf25afbaae
John Jacobsen <jjacobsen@opinionlab.com>
2015-02-19 17:55:26 -0600

    Cleanup / refactor.  Add focus onto text fields.

--- src/ptg/core.clj
+++ src/ptg/core.clj
@@ -56,63 +56,67 @@
        margin-side (+ margin-side pixels-horiz-on-support)])}))
 
 
-(def f (see/frame))
-(def support-width 11)
-(def support-height 5)
-(def support-grid-spacing 1)
-(see/config! f :title "hi")
-(see/config! f :content (see/text :text "da bazzr"))
-(def width-txt (see/text (str support-width)))
-(def height-txt (see/text (str support-height)))
-(def grid-txt (see/text (str support-grid-spacing)))
+(def default-support-width 11)
+(def default-support-height 5)
+(def default-support-grid-spacing 1)
 
 
-(def lbl (see/label))
+(defn- line-x [img y x0 x1]
+  (doseq [x (range x0 x1)]
+    (.setRGB img x y 0)))
 
 
-(def input-form
-  (see/horizontal-panel
-   :items
-   [(see/label :text " Support size: ") width-txt
-    (see/label :text " inches/cm wide by ") height-txt
-    (see/label :text " inches/cm high. Square size: ") grid-txt
-    (see/label :text " inch/cm. ")]))
+(defn- line-y [img x y0 y1]
+  (doseq [y (range y0 y1)]
+    (.setRGB img x y 0)))
 
 
-(def img-scroll (see/scrollable lbl))
+(let [f (see/frame)
+      width-txt (see/text (str default-support-width))
+      height-txt (see/text (str default-support-height))
+      grid-txt (see/text (str default-support-grid-spacing))
+      lbl (see/label)
 
+      input-form
+      (see/horizontal-panel
+       :items
+       [(see/label :text " Support size: ") width-txt
+        (see/label :text " inches/cm wide by ") height-txt
+        (see/label :text " inches/cm high. Square size: ") grid-txt
+        (see/label :text " inch/cm. ")])
 
-(defn draw-image-with-lines [support-width
-                             support-height
-                             support-grid-spacing]
-  (let [img (->> "img.jpg"
-                 clojure.java.io/resource
-                 img/load-image)
-        {:keys [xlines ylines]}
-        (gridlines support-width support-height support-grid-spacing
-                   (.getWidth img) (.getHeight img))
-        contents (see/vertical-panel :items [input-form img-scroll])]
-    (doseq [[y x0 x1] xlines] (line-x img y x0 x1))
-    (doseq [[x y0 y1] ylines] (line-y img x y0 y1))
-    (see/config! f :content contents)
-    (see/config! lbl :icon img)
-    (see/pack! f)
-    (see/show! f)))
+      draw-image-with-lines
+      (fn [support-width
+           support-height
+           support-grid-spacing]
+        (let [img (->> "img.jpg"
+                       clojure.java.io/resource
+                       img/load-image)
+              {:keys [xlines ylines]}
+              (gridlines support-width support-height support-grid-spacing
+                         (.getWidth img) (.getHeight img))
+              contents (see/vertical-panel :items [input-form
+                                                   (see/scrollable lbl)])]
+          (doseq [[y x0 x1] xlines] (line-x img y x0 x1))
+          (doseq [[x y0 y1] ylines] (line-y img x y0 y1))
+          (see/config! f :content contents)
+          (see/config! lbl :icon img)
+          (see/pack! f)
+          (see/show! f)
+          (see/request-focus! width-txt)))
 
+      callback
+      (fn [e]
+        (if (= 10 (.getKeyCode e))
+          (let [width (Double/parseDouble (see/config width-txt :text))
+                height (Double/parseDouble (see/config height-txt :text))
+                grid-size (Double/parseDouble (see/config grid-txt :text))]
+            (draw-image-with-lines width height grid-size))))]
 
-(defn callback [e]
-  (if (= 10 (.getKeyCode e))
-    (let [width (Integer. (see/config width-txt :text))
-          height (Integer. (see/config height-txt :text))
-          grid-size (Integer. (see/config grid-txt :text))]
-      (draw-image-with-lines width height grid-size))))
+  (see/config! f :title "My Image")
+  (see/listen width-txt :key-pressed callback)
+  (see/listen height-txt :key-pressed callback)
+  (see/listen grid-txt :key-pressed callback)
 
-
-(see/listen width-txt :key-pressed callback)
-(see/listen height-txt :key-pressed callback)
-(see/listen grid-txt :key-pressed callback)
-
-
-(see/native!)
-(draw-image-with-lines 12 10 3)
-
+  (see/native!)
+  (draw-image-with-lines 12 10 3))
